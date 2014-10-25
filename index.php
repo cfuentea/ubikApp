59f615e41f022a99a9ded8f476ea0ba76240f9b5
Carlos Fuentealba <carlosfuentealba@gmail.com>
2014-10-25 03:41:44 -0300

    Modifique los comentarios

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-// Librarys to include
+// Librerias externas
 include('errors.php');
 
-// Show errors
+// Mostramos errores
 ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(-1);
@@ -10,22 +10,22 @@ header('Content-Type: application/json');
 
 class RedeemAPI { 
         private $db;
-	// Constructor - open DB connection
+	// Constructor - abrir conexion BBDD
         function __construct() {
                 $this->db = new mysqli('localhost', 'root', '', 'ubikApp');
                 $this->db->autocommit(FALSE); 
         }   
-        // Destructor - close DB connection
+        // Destructor - cerrar conexion BBDD
         function __destruct() {
         	$this->db->close();
         }   
-// Main method to redeem a code
+
 function redeem() {
-// Check for required parameters
+// Verificar que existan los parametros requeridos
 if(isset($_POST["id"])) {
     // Variables que vendran en metodo $_POST
     $userId = $_POST["id"];       
-    // Look up code in database
+    // Preparar query
     $stmt = $this->db->prepare('SELECT id, nombres, apellidos,email,fechaNacimiento FROM Usuario WHERE id = ?');
     $stmt->bind_param("i", $userId);
     $stmt->execute();
