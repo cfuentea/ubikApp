1bbe2765086212dbf775df579b81e24a833ebd2c
Carlos Fuentealba <carlosfuentealba@gmail.com>
2014-10-28 11:50:24 -0300

    Modificación estructura de datos
    Modifiqué la forma en que se consultan los datos y como devolver las
    llamadas, adicionalmente se cambió el método de conexión a PDO

---
+++ Controlador.php
@@ -0,0 +1,56 @@
+<?php
+include('errors.php'); /* Include librerias con códigos de errores */
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(-1);
+header('Content-Type: application/json');
+
+function __autoload($className) {
+	include("$className.php");
+}
+	$datos = new Funciones("localhost","root","","UbikApp");
+
+function mostrar() {
+
+	if(!isset($_POST["id"])) {
+		sendResponse(400, 'Invalid request'); 
+		return false;
+	} 
+		else 
+	{
+		switch($_POST['action']) {
+			case 'getUsers':
+				print $users->getUsers();		
+			break;
+		}
+		/*
+		$stmt = $this->db->prepare('SELECT id, nombres, apellidos,email,fechaNacimiento FROM Usuario WHERE id = ?');
+		$stmt->bind_param("i", $userId);
+		$stmt->execute();
+		$stmt->bind_result($id, $nombres, $apellidos,$email,$fechaNacimiento);
+		if($stmt->fetch()) {
+			$resultado = array(
+				"id" => $id,
+				"nombre" => $nombres,
+				"apellidos" => $apellidos,
+				"email" => $email,
+				"fechaNacimiento" => $fechaNacimiento,
+			);
+			
+			// 	Muestro el resultado según codigo HTML 200 OK, 
+			//	Adicionalmente se codifica a JSON y se le cambia el Header Type a formato XML
+					
+			sendResponse(200, json_encode($resultado));
+			return true;
+		} 
+		*/
+		$stmt->close();
+	
+	}
+
+// Modificado debido a cambio en forma en que se realizan consultas
+
+// $app = new Resultado;
+// $app->mostrar();
+
+?>
---
+++ Funciones.php
@@ -0,0 +1,34 @@
+<?php
+include('errors.php'); /* Include librerias con códigos de errores */
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(-1);
+header('Content-Type: application/json');
+
+class Funciones {
+	
+	private $db;
+		
+	function __construct() {
+			/* Comentado para crear usar PDO
+			$this->db = new mysqli('localhost', 'root', '', 'ubikApp');
+			$this->db->autocommit(FALSE);*/ 
+			$this->db = new PDO("mysql:host=".$host.";dbname=".$db,$user,$pass);
+	}   
+	
+	function __destruct() {
+		$this->db->close();
+	}   
+		
+	public function getUsers($userId){				
+		$sth = $this->db->prepare("SELECT id, nombres, apellidos,email,fechaNacimiento FROM Usuario WHERE id = ?");
+		$sth->bind_param("i", $userId);
+		$sth->execute();
+		//return json_encode($sth->fetchAll());
+		return sendResponse(200, json_encode($resultado));
+		return true;
+	}
+	
+}
+
+?>
