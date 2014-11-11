299950ab018d2f79bd71d3a93964f24f745a71fc
Carlos Fuentealba <carlosfuentealba@gmail.com>
2014-11-10 23:13:19 -0300

    mas cambios

---
+++ lib/db.conf.php
@@ -0,0 +1,8 @@
+<?php
+
+define("HOST", "localhost");
+define("USER", "ubikapp_user");
+define("PASSWORD", "1q2w3e4r");
+define("DATABASE", "ubikapp_main");
+
+?>
---
+++ lib/db.connect.php
@@ -0,0 +1,6 @@
+<?php
+
+include_once('db.conf.php');
+$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
+
+?>
