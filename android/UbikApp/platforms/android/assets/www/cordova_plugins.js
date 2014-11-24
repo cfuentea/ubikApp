8f3fc1f44ae683fa1c3e2f5b9dd0f50a4b7d88bf
TaylerD <cristianyanez.cl@gmail.com>
2014-11-24 00:35:29 -0300

    Info device + SQLite access
    Info device + SQLite access

--- android/UbikApp/platforms/android/assets/www/cordova_plugins.js
+++ android/UbikApp/platforms/android/assets/www/cordova_plugins.js
@@ -13,13 +13,21 @@ module.exports = [
         "merges": [
             "navigator.notification"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "org.apache.cordova.geolocation": "0.3.10",
-    "org.apache.cordova.dialogs": "0.2.10"
+    "org.apache.cordova.dialogs": "0.2.10",
+    "org.apache.cordova.device": "0.2.12"
 }
 // BOTTOM OF METADATA
 });
