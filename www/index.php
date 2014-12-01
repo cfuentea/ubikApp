97f9383c386503f7277aa4f649bca739460b2f77
Carlos Fuentealba <carlosfuentealba@gmail.com>
2014-11-30 21:35:36 -0300

    Graph ok
    el gráfico quedó ok, pero creo que vamos a tener que cambiar la
    librería a Google Charts, ya que al parecer es mas sencilla de utilizar
    y modificar.

--- www/index.php
+++ www/index.php
@@ -180,46 +180,17 @@ if($_SESSION['userId']==0) {
                 <div class="col-lg-8">
                     <div class="panel panel-default">
                         <div class="panel-heading">
-                            <i class="fa fa-bar-chart-o fa-fw"></i> Area Chart Example
-                            <div class="pull-right">
-                                <div class="btn-group">
-                                    <button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown">
-                                        Actions
-                                        <span class="caret"></span>
-                                    </button>
-                                    <ul class="dropdown-menu pull-right" role="menu">
-                                        <li><a href="#">Action</a>
-                                        </li>
-                                        <li><a href="#">Another action</a>
-                                        </li>
-                                        <li><a href="#">Something else here</a>
-                                        </li>
-                                        <li class="divider"></li>
-                                        <li><a href="#">Separated link</a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
+                            <i class="fa fa-bar-chart-o fa-fw"></i> Estado de Campa&ntilde;as                         
                         </div>
                         <!-- /.panel-heading -->
                         <div class="panel-body">
                             <div id="morris-area-chart"></div>
-							 <div id="mapa" style="width:500px;height:250px;"></div>
+                            <div id="morris-donut-chart"></div>
+							<!-- <div id="mapa" style="width:500px;height:250px;"></div> -->
                         </div>
                         <!-- /.panel-body -->
                     </div>
                     <!-- /.table-responsive -->
-                    <!-- /.panel -->
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <i class="fa fa-bar-chart-o fa-fw"></i> Donut Chart Example
-                        </div>
-                        <div class="panel-body">
-                            <div id="morris-donut-chart"></div>
-                            <a href="#" class="btn btn-default btn-block">View Details</a>
-                        </div>
-                        <!-- /.panel-body -->
-                    </div>
                     <!-- /.panel -->
                     </div>
                 </div>
